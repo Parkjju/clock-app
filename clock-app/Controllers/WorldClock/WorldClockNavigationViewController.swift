084c0c55a4c7454720d891569ce5e38a165f8713
Parkjju <rudwns3927@gmail.com>
2023-02-13 18:11:05 +0900

    chore: UIClockNavigationViewController 파일 생성

---
+++ clock-app/Controllers/WorldClock/WorldClockNavigationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WorldClockNavigationViewController.swift
+//  clock-app
+//
+//  Created by 박경준 on 2023/02/13.
+//
+
+import UIKit
+
+class WorldClockNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
