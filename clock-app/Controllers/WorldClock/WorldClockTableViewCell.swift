437234a5c1cb01dec74f5becb0fc9e614b76bcef
Parkjju <rudwns3927@gmail.com>
2023-02-13 18:12:41 +0900

    feat: 세계시간 메인화면 테이블뷰 셋업

---
+++ clock-app/Controllers/WorldClock/WorldClockTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WorldClockTableViewCell.swift
+//  clock-app
+//
+//  Created by 박경준 on 2023/02/13.
+//
+
+import UIKit
+
+class WorldClockTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
