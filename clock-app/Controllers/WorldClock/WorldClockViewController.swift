f9ef118aba393fc47268525f2a099f3d06e1f272
Parkjju <rudwns3927@gmail.com>
2023-02-13 18:14:26 +0900

    feat: +버튼 클릭시 타임존 데이터 셋업 및 네비게이션 바 전달

--- clock-app/Controllers/WorldClock/WorldClockViewController.swift
+++ clock-app/Controllers/WorldClock/WorldClockViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class WorldClockViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        setupController()
     }
     
     func setupNavigationBar(){
@@ -33,12 +36,62 @@ class WorldClockViewController: UIViewController {
     }
     
     @objc func rightBarButtonTapped(){
-        let selectVC = storyboard?.instantiateViewController(withIdentifier: "WorldClockSelectNavigationController") as! UINavigationController
+        let selectVC = storyboard?.instantiateViewController(withIdentifier: "WorldClockSelectNavigationController") as! WorldClockSelectNavigationViewController
         
+        selectVC.clockData = getWorldClockData()
         
         self.navigationController?.present(selectVC, animated: true)
     }
 
+    
+    func setupController(){
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func getWorldClockData() -> [(String, String)]{
+        var worldClickArray:[(String, String)] = []
+        for tz in TimeZone.knownTimeZoneIdentifiers{
+            guard let timezone = TimeZone(identifier: tz) else {
+                continue
+            }
+            guard var worldName = timezone.localizedName(for: .shortGeneric, locale: Locale(identifier:"ko-KR")) else {
+                continue
+            }
+            
+            var data = worldName.split(separator: " ")
+            let _ = data.popLast()
+            
+            worldName = data.joined()
+            
+            let worldDate = Date()
+            var selectedWorld = Date.FormatStyle.dateTime
+            selectedWorld.timeZone = timezone
+            
+            worldClickArray.append((worldName, worldDate.formatted(selectedWorld)))
+//            print(worldName)
+//            print(worldDate.formatted(selectedWorld))
+        }
+        
+        return worldClickArray
+    }
 
 }
 
+extension WorldClockViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WorldClockCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+}
+
+extension WorldClockViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
