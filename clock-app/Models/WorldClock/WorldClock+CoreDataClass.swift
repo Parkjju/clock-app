a8759f0d0baa8eeee8394ebc3e7c3550470bf1ab
Parkjju <rudwns3927@gmail.com>
2023-02-14 18:24:11 +0900

    feat: NSManageObject 서브클래스 파일 추가

---
+++ clock-app/Models/WorldClock/WorldClock+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  WorldClock+CoreDataClass.swift
+//  clock-app
+//
+//  Created by 박경준 on 2023/02/14.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(WorldClock)
+public class WorldClock: NSManagedObject {
+
+}
