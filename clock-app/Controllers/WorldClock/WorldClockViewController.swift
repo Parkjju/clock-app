c0ee4907e13c82e8bae80bd613959612320f9387
Parkjju <rudwns3927@gmail.com>
2023-02-14 12:14:14 +0900

    feat: location ID GET 로직 구현중

--- clock-app/Controllers/WorldClock/WorldClockViewController.swift
+++ clock-app/Controllers/WorldClock/WorldClockViewController.swift
@@ -51,27 +51,63 @@ class WorldClockViewController: UIViewController {
     
     func getWorldClockData() -> [(String, String)]{
         var worldClickArray:[(String, String)] = []
+        
+        
         for tz in TimeZone.knownTimeZoneIdentifiers{
             guard let timezone = TimeZone(identifier: tz) else {
                 continue
             }
-            guard var worldName = timezone.localizedName(for: .shortGeneric, locale: Locale(identifier:"ko-KR")) else {
+            
+            guard let GMT = timezone.abbreviation() else {
                 continue
             }
             
-            var data = worldName.split(separator: " ")
+            
+            guard var regionName = timezone.localizedName(for: .shortGeneric, locale: Locale(identifier:"ko-KR")) else {
+                continue
+            }
+            
+            guard var regionENGName = timezone.localizedName(for: Foundation.NSTimeZone.NameStyle.standard
+                                                             , locale: Locale(identifier:"en-US")) else {
+                continue
+            }
+            
+            
+            // TODO : 타임존 한글명 알아내기.
+            print(timezone.identifier)
+            
+            // GMT + -로 시차 알아낼 수 있음.
+//            guard var abbreviationName = timezone.localizedName(for: Foundation.NSTimeZone.NameStyle.shortStandard, locale: Locale(identifier:"ko-KR")) else {
+//                continue
+//            }
+            
+//            print(regionName)
+//            print(timezone.identifier)
+//            print("")
+            
+            var data = regionName.split(separator: " ")
             let _ = data.popLast()
             
-            worldName = data.joined()
+            regionName = data.joined()
             
             let worldDate = Date()
             var selectedWorld = Date.FormatStyle.dateTime
             selectedWorld.timeZone = timezone
+            print(selectedWorld)
             
-            worldClickArray.append((worldName, worldDate.formatted(selectedWorld)))
-//            print(worldName)
-//            print(worldDate.formatted(selectedWorld))
+            worldClickArray.append((regionName, worldDate.formatted(selectedWorld)))
+            
         }
+        // 현재까지
+        // 1. 타임존에 따른 나라 이름은 얻어냈음
+        // 2. 타임존이 만들어진 지역 이름을 알아내야함.
+        // 3. 지역 이름도 사실 타임존에 걸쳐 있긴 한데 이걸 한국어로 바꿔주는 로직이 있는지
+        
+        // 1. 타임존 identifier중에 language identifier 있는지
+        // 2. 있으면 language identifier 얻어내서 Locale 생성
+        // 3. region 얻어내는 함수 찾아보기
+        
+        
         
         return worldClickArray
     }
