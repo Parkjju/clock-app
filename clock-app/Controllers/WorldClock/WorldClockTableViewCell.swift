0a15c4c02f234a450794854ee624823e284eb42a
Parkjju <rudwns3927@gmail.com>
2023-02-14 18:23:42 +0900

    feat: UITableViewCell IBOutlet 레이블 연결 / 데이터 didSet 로직에 따라 레이블 텍스트 세팅 로직 - Helper함수 구분 필요

--- clock-app/Controllers/WorldClock/WorldClockTableViewCell.swift
+++ clock-app/Controllers/WorldClock/WorldClockTableViewCell.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 class WorldClockTableViewCell: UITableViewCell {
-
+    let worldClockManager = WorldClockManager.shared
+    
+    var clockData: WorldClockData?{
+        didSet{
+            setLabel()
+        }
+    }
+    
+    @IBOutlet weak var offsetLabel: UILabel!
+    @IBOutlet weak var regionLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var noon: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -18,6 +31,23 @@ class WorldClockTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func setLabel(){
+        guard let clock = clockData else {
+            print("Clock data load error in worldClockTableViewCell")
+            return
+        }
+        
+        guard let tz = clock.timezone else {
+            return
+        }
+        
+        regionLabel.text = clock.region
+        timeLabel.text = worldClockManager.getTimeFromTimeZone(timezone: tz, isNoon: false)
+        offsetLabel.text =  worldClockManager.getOffsetFromTimeZone(timezone: tz)
+        noon.text = worldClockManager.getTimeFromTimeZone(timezone: tz, isNoon: true)
+        
     }
 
 }
--- clock-app/Controllers/WorldClock/WorldClockViewController.swift
+++ clock-app/Controllers/WorldClock/WorldClockViewController.swift
@@ -11,6 +11,8 @@ class WorldClockViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    let clockDataManager = WorldClockManager.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
@@ -36,11 +38,11 @@ class WorldClockViewController: UIViewController {
     }
     
     @objc func rightBarButtonTapped(){
-        let selectVC = storyboard?.instantiateViewController(withIdentifier: "WorldClockSelectNavigationController") as! WorldClockSelectNavigationViewController
+        let selectNavigationVC = storyboard?.instantiateViewController(withIdentifier: "WorldClockSelectNavigationController") as! WorldClockSelectNavigationViewController
         
-        selectVC.clockData = getWorldClockData()
+        selectNavigationVC.clockData = clockDataManager.getWorldClockData()
         
-        self.navigationController?.present(selectVC, animated: true)
+        self.navigationController?.present(selectNavigationVC, animated: true)
     }
 
     
@@ -48,81 +50,19 @@ class WorldClockViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
     }
-    
-    func getWorldClockData() -> [(String, String)]{
-        var worldClickArray:[(String, String)] = []
-        
-        
-        for tz in TimeZone.knownTimeZoneIdentifiers{
-            guard let timezone = TimeZone(identifier: tz) else {
-                continue
-            }
-            
-            guard let GMT = timezone.abbreviation() else {
-                continue
-            }
-            
-            
-            guard var regionName = timezone.localizedName(for: .shortGeneric, locale: Locale(identifier:"ko-KR")) else {
-                continue
-            }
-            
-            guard var regionENGName = timezone.localizedName(for: Foundation.NSTimeZone.NameStyle.standard
-                                                             , locale: Locale(identifier:"en-US")) else {
-                continue
-            }
-            
-            
-            // TODO : 타임존 한글명 알아내기.
-            print(timezone.identifier)
-            
-            // GMT + -로 시차 알아낼 수 있음.
-//            guard var abbreviationName = timezone.localizedName(for: Foundation.NSTimeZone.NameStyle.shortStandard, locale: Locale(identifier:"ko-KR")) else {
-//                continue
-//            }
-            
-//            print(regionName)
-//            print(timezone.identifier)
-//            print("")
-            
-            var data = regionName.split(separator: " ")
-            let _ = data.popLast()
-            
-            regionName = data.joined()
-            
-            let worldDate = Date()
-            var selectedWorld = Date.FormatStyle.dateTime
-            selectedWorld.timeZone = timezone
-            print(selectedWorld)
-            
-            worldClickArray.append((regionName, worldDate.formatted(selectedWorld)))
-            
-        }
-        // 현재까지
-        // 1. 타임존에 따른 나라 이름은 얻어냈음
-        // 2. 타임존이 만들어진 지역 이름을 알아내야함.
-        // 3. 지역 이름도 사실 타임존에 걸쳐 있긴 한데 이걸 한국어로 바꿔주는 로직이 있는지
-        
-        // 1. 타임존 identifier중에 language identifier 있는지
-        // 2. 있으면 language identifier 얻어내서 Locale 생성
-        // 3. region 얻어내는 함수 찾아보기
-        
-        
-        
-        return worldClickArray
-    }
-
 }
 
 extension WorldClockViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "WorldClockCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WorldClockCell", for: indexPath) as! WorldClockTableViewCell
+        
+        cell.clockData = clockDataManager.getSavedWorldClock()[indexPath.row]
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return clockDataManager.getSavedWorldClock().count
     }
 }
 
