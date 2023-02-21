e425dd8e786155c432cf8c0b9655c1459c65e6b0
Parkjju <rudwns3927@gmail.com>
2023-02-21 17:51:52 +0900

    feat: 알람 뷰 컨트롤러 Delegate, DataSource 메서드 구현 / 네비게이션 바 rightTap, leftTap 함수 임시 구현

---
+++ clock-app/Controllers/Alarm/AlarmViewController.swift
@@ -0,0 +1,85 @@
+//
+//  WorldClockViewController.swift
+//  clock-app
+//
+//  Created by Jun on 2023/02/09.
+//
+
+import UIKit
+
+class AlarmViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupNavigationBar()
+        setupController()
+        setupUI()
+    }
+    
+    
+    func setupNavigationBar(){
+        self.title = "알람"
+        
+        let leftBarButton = UIBarButtonItem(title: "편집", style: .plain, target: self, action:#selector(leftBarButtonTapped))
+        let rightBarButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(rightBarButtonTapped))
+        
+        leftBarButton.tintColor = .systemOrange
+        rightBarButton.tintColor = .systemOrange
+        
+        self.navigationItem.rightBarButtonItem = rightBarButton
+        self.navigationItem.leftBarButtonItem = leftBarButton
+        
+    }
+    
+    @objc func leftBarButtonTapped(){
+        print("left")
+    }
+    
+    @objc func rightBarButtonTapped(){
+        print("right")
+    }
+    
+    func setupController(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func setupUI(){
+        tableView.backgroundColor = .black
+    }
+
+}
+
+extension AlarmViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+    
+}
+
+extension AlarmViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AlarmTableViewCell", for: indexPath) as? AlarmTableViewCell else {
+            print("HI!")
+            return UITableViewCell()
+        }
+        // 하드코딩
+        let alarm = AlarmData()
+        alarm.isOn = true
+        alarm.noon = "오후"
+        alarm.time = "3:08"
+        alarm.description = "알람"
+        
+        cell.alarmData = alarm
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+}
