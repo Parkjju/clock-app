c387725588ec419cad0553a141f1a3554d2fcacf
Parkjju <rudwns3927@gmail.com>
2023-02-21 17:50:52 +0900

    feat: AlarmData mockup data 생성

---
+++ clock-app/Models/Alarm/AlarmManager.swift
@@ -0,0 +1,16 @@
+//
+//  AlarmManager.swift
+//  clock-app
+//
+//  Created by 박경준 on 2023/02/21.
+//
+
+import Foundation
+
+class AlarmManager{
+    static let shared = AlarmManager()
+    
+    private init(){}
+    
+    
+}
