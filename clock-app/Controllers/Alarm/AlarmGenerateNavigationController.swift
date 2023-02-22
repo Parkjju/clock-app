9d4fcd6f4a8862ba3b8323830ffb6886a8cd5bca
Parkjju <rudwns3927@gmail.com>
2023-02-22 10:18:00 +0900

    chore: AlarmNavigationViewController 생성

---
+++ clock-app/Controllers/Alarm/AlarmGenerateNavigationController.swift
@@ -0,0 +1,27 @@
+//
+//  AlarmGenerateNavigationController.swift
+//  clock-app
+//
+//  Created by Jun on 2023/02/22.
+//
+
+import UIKit
+
+class AlarmGenerateNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
