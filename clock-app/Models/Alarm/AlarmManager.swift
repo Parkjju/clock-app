64145c7343919cd2e73cf2dc9d9fe18bbae57e2c
Parkjju <rudwns3927@gmail.com>
2023-02-23 17:34:45 +0900

    feat: MVC model - AlarmData Manager getAll로직 / save 로직 추가

--- clock-app/Models/Alarm/AlarmManager.swift
+++ clock-app/Models/Alarm/AlarmManager.swift
@@ -5,12 +5,82 @@
 //  Created by 박경준 on 2023/02/21.
 //
 
-import Foundation
+import UIKit
+import CoreData
 
 class AlarmManager{
     static let shared = AlarmManager()
     
     private init(){}
     
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    
+    lazy var context = appDelegate?.persistentContainer.viewContext
+    
+    let modelName = "AlarmData"
+    
+    
+    // 1. 코어데이터에서 저장된 알람정보 전부 가져오기
+    func getSavedAlarm() -> [AlarmData] {
+        var data: [AlarmData] = []
+        guard let context = context else {
+            print("getSavedAlarm: context load error")
+            return []
+        }
+        
+        let request = NSFetchRequest<NSManagedObject>(entityName: "AlarmData")
+        
+        let descriptor = NSSortDescriptor(key: "time", ascending: true)
+        
+        request.sortDescriptors = [descriptor]
+        
+        do{
+            guard let fetchedData = try context.fetch(request) as? [AlarmData] else {
+                print("getSavedAlarm: fetch error")
+                return data
+            }
+            
+            data = fetchedData
+        }catch{
+            print("error")
+        }
+        
+        return data
+    }
+    
+    // 2. 코어데이터에 알람정보 저장하기
+    func saveAlarm(newData: AlarmData, completion: @escaping () -> Void){
+        guard let context = context else {
+            print("saveAlarm: context load error")
+            return
+        }
+        
+        guard let entity = NSEntityDescription.entity(forEntityName:self.modelName , in: context) else {
+            return
+        }
+        
+        guard let newAlarm = NSManagedObject(entity: entity, insertInto: context) as? AlarmData else {
+            print("saveAlarm: entity insert error")
+            return
+        }
+        
+        newAlarm.isOn = newData.isOn
+        newAlarm.time = newData.time
+        newAlarm.label = newData.label
+        newAlarm.isAgain = newData.isAgain
+        newAlarm.isRepeat = newData.isRepeat
+        newAlarm.sound = newData.sound
+        
+        if(context.hasChanges){
+            do{
+                try context.save()
+                completion()
+            }catch{
+                print("saveAlarm: context save error")
+                completion()
+            }
+        }
+        
+    }
     
 }
