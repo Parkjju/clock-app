0ed92bf8da60528fedb7115dfe02e7675b2aeaaa
Parkjju <rudwns3927@gmail.com>
2023-02-24 17:27:54 +0900

    feat: 커스텀 델리게이트 패턴 세팅 - 호출 뷰 컨트롤러 데이터 세팅 및 델리게이트 프로토콜 로직 구현

--- clock-app/Controllers/Alarm/AlarmGenerateViewController.swift
+++ clock-app/Controllers/Alarm/AlarmGenerateViewController.swift
@@ -15,6 +15,25 @@ class AlarmGenerateViewController: UIViewController {
     
     var newAlarmData: AlarmData?
     
+    let dict: [Int: String] = [
+        0: "일",
+        1: "월",
+        2: "화",
+        3: "수",
+        4: "목",
+        5: "금",
+        6: "토"
+    ]
+    
+    var checkedDayList: [Int: Bool] = [
+        0: false,
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+        5: false,
+        6: false,
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -125,4 +144,46 @@ extension AlarmGenerateViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
+
+extension AlarmGenerateViewController: AlarmManagerDelegate{
+    
+    func repeatUpdate(index: Int) {
+        guard let repeatTableViewCell = tableView.visibleCells[0] as? AlarmSettingRepeatTableViewCell else {
+            return
+        }
+        
+        
+        
+        repeatTableViewCell.titleLabel.text = ""
+        
+        // 1. 커스텀 델리게이트 패턴으로 체크마크 요일 목록을 받아온다
+        // 2. 체크 목록을 Boolean 타입 값으로 딕셔너리 체킹을 하고
+        // 3. 최종적으로 레이블을 바꿔준다.
+        if(checkedDayList[index]!){
+            checkedDayList[index] = false
+        }else{
+            checkedDayList[index] = true
+        }
+        
+        for checkedDay in checkedDayList{
+            if(checkedDay.value){
+                repeatTableViewCell.titleLabel.text?.append(contentsOf: "\(dict[checkedDay.key]!) ")
+            }
+        }
+        
+        // 아무것도 체크되지 않았으면 안함 텍스트로 변경
+        if(repeatTableViewCell.titleLabel.text?.count == 0){
+            repeatTableViewCell.titleLabel.text = "안 함"
+        }
+        
+        tableView.reloadData()
+    }
+        
+}
+    
+
