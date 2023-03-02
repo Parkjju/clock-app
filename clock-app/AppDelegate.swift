f9abd35d8273a4c38c3847db39f5979b084489ee
Parkjju <rudwns3927@gmail.com>
2023-03-02 11:31:05 +0900

    feat: 코어데이터 mergedModel 인스턴스 생성 및 컨테이너 메서드 managedObjectModel 파라미터 호출

--- clock-app/AppDelegate.swift
+++ clock-app/AppDelegate.swift
@@ -42,7 +42,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "clock_app")
+        let modelBundles: [Bundle] = [
+            Bundle(for: AlarmData.self),
+            Bundle(for: WorldClockData.self)
+        ]
+        let model = NSManagedObjectModel.mergedModel(from: modelBundles)!
+        
+        
+        let container = NSPersistentContainer(name: "clock_app", managedObjectModel: model)
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
