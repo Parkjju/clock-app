b6c68466deed7d5b4e6bdc569aea9f899cd1d383
Parkjju <rudwns3927@gmail.com>
2023-03-07 08:39:45 +0900

    feat: 기존 노티피케이션 API 코드 삭제

--- clock-app/AppDelegate.swift
+++ clock-app/AppDelegate.swift
@@ -11,39 +11,38 @@ import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate{
-
-
+    
+    
     // 앱 런치스크린 띄운 뒤 푸시알람 권한 요청
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         sleep(2)
-        registerForPushNotifications()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
         let modelBundles: [Bundle] = [
             Bundle(for: AlarmData.self),
             Bundle(for: WorldClockData.self)
@@ -56,7 +55,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -70,9 +69,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
         })
         return container
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -86,35 +85,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
             }
         }
     }
-
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate{
-    // 푸시알람 권한요청 함수
-    func registerForPushNotifications() {
-          UNUserNotificationCenter.current().delegate = self
-          UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
-              (granted, error) in
-              print("Permission granted: \(granted)")
-              // 1. Check if permission granted
-              guard granted else { return }
-              // 2. Attempt registration for remote notifications on the main thread
-              DispatchQueue.main.async {
-                  UIApplication.shared.registerForRemoteNotifications()
-              }
-          }
-      }
     
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let tokenParts = deviceToken.map { data -> String in
-            return String(format: "%02.2hhx", data)
-        }
-        let token = tokenParts.joined()
-        print("Device Token: \(token)")
-    }
-    
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("Failed to register for remote notifications with error: \(error)")
-    }
-
 }
