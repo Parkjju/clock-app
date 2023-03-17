ca928ec4f4a194577209bb87df0268c5f9f54f32
Parkjju <rudwns3927@gmail.com>
2023-03-17 21:28:32 +0900

    feat: 초성에서 자모로 유니코드 변환하는 함수

--- clock-app/Util/ParsingJaeum.swift
+++ clock-app/Util/ParsingJaeum.swift
@@ -7,6 +7,37 @@
 
 import Foundation
 
-func parsing(str: String){
-    
+func convertJamoToChosung(jamo: String) -> String {
+    switch jamo.unicodeScalars.first!.value{
+    case 4352: // 초성 ㄱ을 자모 ㄱ으로
+        return "ㄱ"
+    case 4354:
+        return "ㄴ"
+    case 4355:
+        return "ㄷ"
+    case 4357:
+        return "ㄹ"
+    case 4358:
+        return "ㅁ"
+    case 4359:
+        return "ㅂ"
+    case 4361:
+        return "ㅅ"
+    case 4363:
+        return "ㅇ"
+    case 4364:
+        return "ㅈ"
+    case 4366:
+        return "ㅊ"
+    case 4367:
+        return "ㅋ"
+    case 4368:
+        return "ㅌ"
+    case 4369:
+        return "ㅍ"
+    case 4370:
+        return "ㅎ"
+    default:
+        return ""
+    }
 }
