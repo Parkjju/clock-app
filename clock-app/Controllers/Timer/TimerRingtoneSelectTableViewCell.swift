610266e49f958e3b78ddd06af550e88d89207ed5
Parkjju <rudwns3927@gmail.com>
2023-04-03 09:50:35 +0900

    feat: 타이머 알람음 테이블뷰셀 클래스 생성

---
+++ clock-app/Controllers/Timer/TimerRingtoneSelectTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TimerRingtoneSelectTableViewCell.swift
+//  clock-app
+//
+//  Created by Jun on 2023/04/03.
+//
+
+import UIKit
+
+class TimerRingtoneSelectTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var soundLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setupUI(){
+        self.backgroundColor = UIColor(named: "ModalSettingTableViewColor")
+        soundLabel.textColor = .white
+    }
+
+}
