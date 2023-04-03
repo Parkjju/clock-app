9304fb5d2662bef1df55a5f022ff0a64b43e42b7
Parkjju <rudwns3927@gmail.com>
2023-04-03 09:51:28 +0900

    feat: 타이머 알람음 모달창 생성 / 알람음 목록 저장속성 추가 / 테이블뷰 delegate, datasource 추가

---
+++ clock-app/Controllers/Timer/TimerRingtoneSelectViewController.swift
@@ -0,0 +1,74 @@
+//
+//  TimerRingtoneSelectViewController.swift
+//  clock-app
+//
+//  Created by Jun on 2023/04/03.
+//
+
+import UIKit
+
+class TimerRingtoneSelectViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let sounds: [String] = ["daydream", "green", "playTime", "sea"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+        setupController()
+    }
+    
+    func setupUI(){
+        view.backgroundColor = UIColor(named: "ModalColor")
+        tableView.backgroundColor = UIColor(named:"ModalSettingTableViewColor")
+        tableView.clipsToBounds = true
+        tableView.layer.cornerRadius = 10
+        tableView.heightAnchor.constraint(equalToConstant: CGFloat(50 * sounds.count)).isActive = true
+        
+        let leftBarButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelButtonTapped))
+        let rightBarButton = UIBarButtonItem(title: "설정", style: .plain, target: self, action: #selector(settingButtonTapped))
+        leftBarButton.tintColor = .systemOrange
+        rightBarButton.tintColor = .systemOrange
+        self.navigationItem.leftBarButtonItem = leftBarButton
+        self.navigationItem.rightBarButtonItem = rightBarButton
+    }
+    @objc func cancelButtonTapped(){
+        self.dismiss(animated: true)
+    }
+    @objc func settingButtonTapped(){
+        
+    }
+    
+    func setupController(){
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+}
+
+extension TimerRingtoneSelectViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TimerRingtoneSelectTableViewCell", for: indexPath) as? TimerRingtoneSelectTableViewCell else {
+            return UITableViewCell()
+        }
+        let soundName = translateSoundNameToKorean(text: sounds[indexPath.row])
+        cell.soundLabel.text = soundName
+        
+        return cell
+    }
+}
+
+extension TimerRingtoneSelectViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
