bdc538f4c1ec3aa09877615cdecdb19ffde4a556
Parkjju <rudwns3927@gmail.com>
2023-04-13 08:58:32 +0900

    feat: 세계시간 타임존 저장시 clockDataWithSection 에서 데이터 얻기 / indexPath.row 섹션별 초기화 문제

--- clock-app/Controllers/WorldClock/WorldClockSelectViewController.swift
+++ clock-app/Controllers/WorldClock/WorldClockSelectViewController.swift
@@ -109,7 +109,8 @@ class WorldClockSelectViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = rightBarButton
     }
     
-    @objc func rightBarButtonTapped(){    
+    @objc func rightBarButtonTapped(){
+        print(filteredData)
         self.dismiss(animated: true)
     }
     
@@ -203,7 +204,13 @@ extension WorldClockSelectViewController: UITableViewDelegate{
                 self.dismiss(animated: true)
             }
         }else{
-            worldClockManager.saveWorldClockData(newRegion: clockData[indexPath.row].0, newTimezone: clockData[indexPath.row].1) {
+            
+            print(clockData)
+            // indexPath.row가 섹션마다 0으로 초기화됨
+            // 타임존 명은 clockDataWithSection에서 불러올 수 있는데
+            // 타임존 객체를 불러오는 방법은?
+            
+            worldClockManager.saveWorldClockData(newRegion: clockDataWithSection[sectionTitles[indexPath.section]]![indexPath.row], newTimezone: clockData[indexPath.row].1) {
                 self.dismiss(animated: true)
             }
         }
