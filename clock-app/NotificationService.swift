39701d93215a884aaa796caeb01ce5cd871eba43
Parkjju <rudwns3927@gmail.com>
2023-04-14 09:00:46 +0900

    feat: requestAlarmNotification 함수 파라미터 - dataIndex 추가 / content.userInfo에 알람대상 인덱스 저장 후 willPresent에서 isOn속성 false로 변경하도록 구현

--- clock-app/NotificationService.swift
+++ clock-app/NotificationService.swift
@@ -16,15 +16,10 @@ class NotificationService: NSObject{
     
     let UNCurrentCenter = UNUserNotificationCenter.current()
     
-    // 1. dismissAction 정의
-    // 2. 노티피케이션 카테고리 정의
-    let dismissAction = UNNotificationAction(identifier: "dismissAction", title: "닫기", options: [])
+    var pendingNotificationArray: [UNNotificationRequest]?
     
-    lazy var category = UNNotificationCategory(identifier: "myNotificationCategory", actions: [dismissAction], intentIdentifiers: [], options: []){
-        didSet{
-            UNCurrentCenter.setNotificationCategories([category])
-        }
-    }
+    // AlarmData 모델 매니저에서 현재 보유중인 알람데이터들 불러오기
+    let alarmManager = AlarmManager.shared
     
     func authorizeNotification(){
         let options: UNAuthorizationOptions = [.alert, .badge, .sound]
@@ -43,27 +38,33 @@ class NotificationService: NSObject{
     
     // 전달되는 date는 현재 변경 대상에 대한 날짜값
     // 이전에 전달된 date를 가지고 pendingNotification을 삭제해야됨
-    func requestAlarmNotification(_ date: Date?, type:String,title: String, subtitle: String, sound: String, repeatedly:Bool = false, withInterval interval: TimeInterval?, notificationId: String){
+    func requestAlarmNotification(_ date: Date?, type:String,title: String, subtitle: String, sound: String, repeatedly:Bool = false, withInterval interval: TimeInterval?, notificationId: String, _ dataIndex: Int?){
         
         let content = UNMutableNotificationContent()
         content.title = title
         content.subtitle = subtitle
-        content.categoryIdentifier = notificationId
+        content.categoryIdentifier = "Alarm"
         
         let sound = sound
-        
         content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "\(sound).wav"))
+        
+        // 데이터 인덱스가 Alarm 뷰컨에서 전달되었으면 그대로 언래핑 후 사용
+        // 데이터 인덱스가 Timer 뷰컨에서 전달되었으면 디폴트값 0 전달
+        content.userInfo = ["updateTarget": dataIndex ?? -1]
                 
         // 삼항연산자로 타입캐스팅
         let trigger = type == "Alarm" ?  getTrigger(type: type, date: date,interval: interval) as! UNCalendarNotificationTrigger : getTrigger(type: type, date: date, interval: interval) as! UNTimeIntervalNotificationTrigger
         
         let request = UNNotificationRequest(identifier: notificationId, content: content, trigger: trigger)
+        
+        setCustomAction(actionIdentifier: "dismiss", actionTitle: "알람", categoryIdentifier: "Alarm")
     
         // remove All peding notification requests
         // 타이머를 작동시키는 상황에서 알람 돌리면 기존 노티 삭제됨?
         NotificationService.sharedInstance.UNCurrentCenter.removePendingNotificationRequests(withIdentifiers: [notificationId])
         
         NotificationService.sharedInstance.UNCurrentCenter.add(request)
+        
     }
     
     func getTrigger(type: String, date: Date?, interval: TimeInterval?) -> UNNotificationTrigger?{
@@ -90,18 +91,27 @@ class NotificationService: NSObject{
         default:
             return nil
         }
-
+    }
+    
+    func setCustomAction(actionIdentifier: String, actionTitle: String, categoryIdentifier: String){
+        let customAction = UNNotificationAction(identifier: actionIdentifier, title: actionTitle, options: [])
+        let category = UNNotificationCategory(identifier: categoryIdentifier, actions: [customAction], intentIdentifiers: [])
+        
+        self.UNCurrentCenter.setNotificationCategories([category])
     }
 }
 
 extension NotificationService: UNUserNotificationCenterDelegate{
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
         print(response.actionIdentifier)
         switch response.actionIdentifier{
         case UNNotificationDismissActionIdentifier:
             print("dismiss")
         case UNNotificationDefaultActionIdentifier:
             print("default")
+        case "dismiss":
+            print("custom")
         default:
             break
         }
@@ -109,8 +119,15 @@ extension NotificationService: UNUserNotificationCenterDelegate{
         completionHandler()
     }
     
+    // willPresent에서 isOn속성 제거
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        print("willPresent notification")
+        
+        if(notification.request.content.userInfo["updateTarget"] as! Int != -1){
+            let alarmDataList = alarmManager.getSavedAlarm()
+            alarmDataList[notification.request.content.userInfo["updateTarget"] as! Int - 1].isOn = false
+        }
+        
+        
         
         let options: UNNotificationPresentationOptions = [.banner, .sound]
         completionHandler(options)
